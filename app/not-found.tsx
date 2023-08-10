ec767c5bb35bc3f3674856538ceabd0da856c87f
Landay <landiehan@outlook.com>
2023-08-11 00:08:30 +0800

    feat: add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <main className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-xl">404</h1>
+      <p className="mt-2">
+        This page could not be found.{' '}
+        <span aria-label="emoji: paw prints">🐾</span>
+      </p>
+    </main>
+  )
+}
