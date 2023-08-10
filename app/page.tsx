b2f905f4c27d1754a9fa681b2008da67bb9e15f8
Landay <landiehan@outlook.com>
2023-08-10 23:14:21 +0800

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-xl">Welcome</h1>
+      <p className="mt-2">
+        Cute tools coming soon. <span aria-label="emoji: paw prints">🐾</span>
+      </p>
+    </main>
+  )
+}
